b4d65fd7758805e80309eb023b7ae2acb8b6ae72
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-04-18 18:58:22 +0200

    mamie walk

--- LudumDareUnity/Assets/Scripts/ChangeRoom.cs
+++ LudumDareUnity/Assets/Scripts/ChangeRoom.cs
@@ -5,28 +5,28 @@ public class ChangeRoom : MonoBehaviour {
 
 	public Transform leftDoor;
 	public Transform rightDoor;
-
-	bool moveRight = true;
+	
+	bool moveRight = false;
 	bool moveLeft = false;
-
+	private Vector3 init;
 	Vector3 refVel;
 
 	void Start () 
 	{
-	
+		init = transform.position;
 	}
 	
 	void Update ()
 	{
 		if (moveLeft && !moveRight)
 		{
-			this.transform.position =  Vector3.SmoothDamp(this.transform.position, leftDoor.position, ref refVel, 0.9f);
-			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+			this.transform.localScale = new Vector3(1, 1, 1);
+			this.transform.position = Vector3.Lerp(this.transform.position, new Vector3 (leftDoor.position.x, init.y, init.z), 2 * Time.deltaTime);
 		}
 		if (!moveLeft && moveRight)
 		{
-			this.transform.position = Vector3.SmoothDamp(this.transform.position, rightDoor.position, ref refVel, 0.9f);
-			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+			this.transform.localScale = new Vector3(-1, 1, 1);
+			this.transform.position = Vector3.Lerp(this.transform.position, new Vector3 (rightDoor.position.x, init.y, init.z), 2 * Time.deltaTime);
 		}
 	}
 
---
+++ LudumDareUnity/Assets/door.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+
+	public GameObject exit_door;
+	public bool right;
+	public string roomToGo = "none";
+	void OnMouseDown()
+	{
+		if (right)
+			GameObject.FindGameObjectWithTag ("Player").GetComponent<ChangeRoom> ().MoveRight ();
+		else
+			GameObject.FindGameObjectWithTag ("Player").GetComponent<ChangeRoom> ().MoveLeft();
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.transform.localScale.x == 1)
+		{
+			Camera.main.GetComponent<FadeTransition>().StartCoroutine("Fade", roomToGo);
+			StartCoroutine(wait(col, -1));
+			col.GetComponent<ChangeRoom>().DontMove();
+		}
+		else if (col.transform.localScale.x == - 1)
+		{
+			Camera.main.GetComponent<FadeTransition>().StartCoroutine("Fade", roomToGo);
+			StartCoroutine(wait(col, 1));
+			col.GetComponent<ChangeRoom>().DontMove();
+		}
+	}
+	IEnumerator wait(Collider2D col, int sign)
+	{
+		col.GetComponent<ChangeRoom>().DontMove();
+		yield return new WaitForSeconds (1);
+		col.transform.position = new Vector3(exit_door.transform.position.x + sign * (col.bounds.size.x + 1), col.transform.position.y, col.transform.position.z);
+	}
+}
