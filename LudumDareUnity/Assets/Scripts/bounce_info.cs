bb1a7114be18134f76fde0e21b12f00d0fccbf14
Fernand <fernand.veyrier@epitech.eu>
2015-04-18 14:38:19 +0200

    add sounds

--- LudumDareUnity/Assets/Scripts/bounce_info.cs
+++ LudumDareUnity/Assets/Scripts/bounce_info.cs
@@ -5,20 +5,25 @@ public class bounce_info : MonoBehaviour {
 
 	public Sprite image;
 	public float duration;
+	public AudioClip sound;
 	GameObject obj;
 	Vector3 velocity = Vector3.zero;
+
 	void Start()
 	{
 		obj = new GameObject();
 		obj.transform.position = transform.position;
 		obj.AddComponent<SpriteRenderer>();
 		obj.GetComponent<SpriteRenderer>().sprite = image;
+		obj.AddComponent<AudioSource>().clip = sound;
+		obj.GetComponent<AudioSource>().Play();
 		StartCoroutine("died");
 		obj.transform.localScale = Vector3.zero;
 	}
 	void Update()
 	{
-		obj.transform.localScale = Vector3.Lerp (obj.transform.localScale, Vector3.one, 4 * Time.deltaTime);
+		if (obj)
+			obj.transform.localScale = Vector3.Lerp (obj.transform.localScale, Vector3.one, 4 * Time.deltaTime);
 	}
 	IEnumerator died()
 	{
