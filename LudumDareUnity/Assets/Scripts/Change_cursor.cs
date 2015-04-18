feae08b5d18c0515d4bfe54352c907c17fe4d4bd
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-04-18 05:56:06 +0200

    change curseur and folder

---
+++ LudumDareUnity/Assets/Scripts/Change_cursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Change_cursor : MonoBehaviour {
+
+	public Texture2D texture;
+	void OnMouseEnter()
+	{
+		Cursor.SetCursor (texture, Vector2.zero, CursorMode.Auto);
+	}
+	void OnMouseExit()
+	{
+		Cursor.SetCursor (null, Vector2.zero, CursorMode.Auto);
+	}
+}
