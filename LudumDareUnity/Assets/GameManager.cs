ad3f2c1142526fc24f3e16b9dab113add7cd243b
Fernand <fernand.veyrier@epitech.eu>
2015-04-18 17:17:06 +0200

    tudududutuduuu

---
+++ LudumDareUnity/Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject pausePanel;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+
+}
