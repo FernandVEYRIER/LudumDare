907b23c8bce78b2c01993a40cac5d062e5b435bc
Fernand <fernand.veyrier@epitech.eu>
2015-04-18 17:28:09 +0200

    added pause menu

---
+++ LudumDareUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject pausePanel;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void dispPause()
+	{
+		Time.timeScale = (Time.timeScale == 0) ? 1 : 0;
+		pausePanel.SetActive(!pausePanel.activeSelf);
+	}
+
+	public void quitGame()
+	{
+		Application.Quit();
+	}
+}
