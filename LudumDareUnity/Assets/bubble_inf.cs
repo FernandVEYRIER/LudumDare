33f458c54174a73c3dbbacbdf8bb80edf9295165
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-04-20 00:34:07 +0200

    je push

---
+++ LudumDareUnity/Assets/bubble_inf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class bubble_inf : MonoBehaviour {
+
+	public GameObject canvas;
+	public GameObject parent;
+	void Update ()
+	{
+		float mult;
+		canvas.transform.position = transform.position;
+		if  ((canvas.transform.localScale.x > 0 && parent.transform.localScale.x < 0) || (canvas.transform.localScale.x < 0 && parent.transform.localScale.x > 0))
+			canvas.transform.localScale = new Vector3(canvas.transform.localScale.x * -1, canvas.transform.localScale.y, canvas.transform.localScale.z);
+	}
+	public void create_bubble(Sprite spr)
+	{
+
+	}
+}
