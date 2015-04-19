91d4383c4bc545129b05acd8c4d2c97594be78c0
Fernand <fernand.veyrier@epitech.eu>
2015-04-19 18:48:38 +0200

    anim wdinow

---
+++ LudumDareUnity/Assets/AnimCloud.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimCloud : MonoBehaviour {
+
+	public float velocity = 0.001f;
+
+	void Update () 
+	{
+		this.GetComponent<MeshRenderer>().material.mainTextureOffset += new Vector2(velocity, 0);
+	}
+}
