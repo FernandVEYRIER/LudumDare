ad9bd6679621faedd30afbf0fdd83938a2ad383a
Fernand <fernand.veyrier@epitech.eu>
2015-04-20 13:32:04 +0200

    niom niom

---
+++ LudumDareUnity/Assets/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void loadIntro()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
