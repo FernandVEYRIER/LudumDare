7513f8c321da343c17b27e1d16ac1ff539d59eae
Fernand <fernand.veyrier@epitech.eu>
2015-04-20 22:24:01 +0200

    tamer

---
+++ LudumDareUnity/Assets/Scripts/ChienAttack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChienAttack : MonoBehaviour {
+
+	GameObject target = null;
+
+	void Start () 
+	{
+		target = GameObject.FindGameObjectWithTag("Scout");
+	}
+	
+	void Update () 
+	{
+		while (Mathf.Abs(Vector2.Distance(this.transform.position, target.transform.position)) > 1)
+		{
+			transform.position = Vector3.Lerp(this.transform.position, target.transform.position, 1);
+		}
+	}
+}
