d8508cf9b113af0c866060620225968cd5475178
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-04-20 19:10:42 +0200

    fix anim weapon

---
+++ LudumDareUnity/Assets/anim_weapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim_weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Update ()
+	{
+		this.GetComponent<Animator>().SetBool("move", GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().GetBool("move"));
+	}
+}
