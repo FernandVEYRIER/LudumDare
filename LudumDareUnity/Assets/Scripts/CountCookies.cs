0c3aae9874704e8e91b0cec912dbf9a3155f3c82
Fernand <fernand.veyrier@epitech.eu>
2015-04-21 00:26:52 +0200

    unreachable

--- LudumDareUnity/Assets/Scripts/CountCookies.cs
+++ LudumDareUnity/Assets/Scripts/CountCookies.cs
@@ -7,7 +7,7 @@ public class CountCookies : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		if (PlayerPrefs.GetInt("Cookies") == null)
+		if (PlayerPrefs.GetInt("Cookies", 0) == 0)
 			PlayerPrefs.SetInt("Cookies", 0);
 		this.GetComponent<Text>().text = "Cookies awarded : " + PlayerPrefs.GetInt("Cookies");
 	}
