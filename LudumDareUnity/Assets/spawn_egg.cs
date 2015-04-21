8a9ced6d904a5c792f1f43cb4bcad81ce7357f14
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-04-21 02:05:01 +0200

    pref

--- LudumDareUnity/Assets/spawn_egg.cs
+++ LudumDareUnity/Assets/spawn_egg.cs
@@ -5,12 +5,13 @@ public class spawn_egg : MonoBehaviour {
 
 	public float time = 0.5f;
 	public GameObject projectile;
-	private int count;
+	private int count = 0;
 	private bool activate = false;
 	// Update is called once per frame
 	void Update () 
 	{
-		if (activate = false && count < 3 && GameObject.FindGameObjectWithTag ("Scout").GetComponent<Animator> ().GetInteger ("anim") != 0)
+		print (GameObject.FindGameObjectWithTag ("Scout").GetComponent<Animator> ().GetInteger ("anim"));
+		if (activate = false && count < 3 && GameObject.FindGameObjectWithTag ("Scout").GetComponent<Animator> ().GetInteger ("anim") == 2)
 			StartCoroutine (fire());
 	}
 	IEnumerator fire()
@@ -19,6 +20,7 @@ public class spawn_egg : MonoBehaviour {
 		count++;
 		activate = true;
 		tmp = (GameObject)Instantiate (projectile, transform.position, transform.rotation);
+		print ("plop");
 		tmp.transform.Translate (-1.5f, 0, 0);
 		yield return new WaitForSeconds (time);
 		activate = false;
